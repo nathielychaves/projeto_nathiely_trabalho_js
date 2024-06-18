96379f02abecc886631de35d36146916bb2d4692
nathiely chaves <161589375+nathielychaves@users.noreply.github.com>
2024-06-18 08:49:14 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,30 +2,28 @@ var meuFormulario = document.getElementById("formulario");
 meuFormulario.addEventListener("submit", function(event) {
   event.preventDefault();
 
-  //Chama a função somar
+
   maiuscula();
 
 });
 
 
-//Função somar
 function maiuscula() {
-  console.log("Este é o arquivo correto....será ?????");
-  //Captura o que está dentro do primeiro input no formulário
-  //Palavra - Transforma o texto capturado em Palavra
+
+
   let palavra = document.getElementById("palavra").value;
 
   let primeiraLetra = palavra.charAt(0);
 
-
-  let resultado = primeiraLetra;
-
-  console.log(primeiraLetra);
-
-  if () {}
-
-  //Aqui vai escrever no input "resultado_soma" o valor que está armazenado na variável "resultado_soma"
-  document.getElementById("resultado").value = resultado;
+  // se primeira letra for maiúscula escreva sim
+  if (primeiraLetra == primeiraLetra.toUpperCase()) {
+      document.getElementById("resultado").value = "SIM";
+  } else {
+    document.getElementById("resultado").value = "NÃO";
+  }
 
 
-}-
+
+
+
+}
